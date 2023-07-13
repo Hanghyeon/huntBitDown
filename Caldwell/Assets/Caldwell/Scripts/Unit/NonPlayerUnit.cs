f013b2999e97f4010e09ceb60520f99ff39cc781
Hanghyeon <whgkdgus56@gmail.com>
2023-07-13 22:22:39 +0900

    make Units

---
+++ Caldwell/Assets/Caldwell/Scripts/Unit/NonPlayerUnit.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Unit
+{
+    public abstract class NonPlayerUnit : MonoBehaviour, InterfaceUnit
+    {
+        public enum State
+        {
+            Sleep,      // 비활성 상태
+            Alert,      // 경고 상태
+            Roaming,    // 대상을 잃어버리고 찾는 상태 -> 인게이징에 들어가면 로밍 아래로 내려가지 않음
+            Engauge     // 대상을 발견하고 해당 위치로 이동하여 공격하는 상태
+        }
+
+        public int MaxHP { get; protected set; }
+        public int CurHP { get; set; }
+
+        public State CurState { get; protected set; }
+
+
+        protected virtual void SetState(State _state)
+        {
+            if (State.Roaming <= CurState)
+            {
+                if (State.Roaming <= _state)
+                    CurState = _state;
+            }
+            else
+            {
+                if (State.Roaming > _state)
+                    CurState = _state;
+            }
+        }
+    }
+}
